dda4a92c87b9fe7024740fd1f9fe0a883cb167cc
vivacee <yaozhongchen@gmail.com>
2017-07-02 16:55:47 -0500

    init

---
+++ Employee/src/my/intpro/com/App.java
@@ -0,0 +1,16 @@
+package my.intpro.com;
+
+import java.util.ArrayList;
+
+/**
+ * Created by aaron on 7/1/2017.
+ */
+public class App {
+    public static void main(String [] args){
+        //ArrayList<Employee> listEmployee = new ArrayList<Employee>();
+       // Employee emp1 = new Employee();
+
+        System.out.println("---app started.....");
+
+    }
+}
---
+++ Employee/src/my/intpro/com/Employee.java
@@ -0,0 +1,14 @@
+package my.intpro.com;
+
+import java.util.ArrayList;
+
+/**
+ * Created by aaron on 7/1/2017.
+ */
+public class Employee {
+    String name; //LastName, FirstName
+    String department;
+    int age;
+
+
+}
